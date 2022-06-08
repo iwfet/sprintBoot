4548d699740ad35b779738a031bb22ef64bc3420
iwfet <j13vvc@gmail.com>
2022-06-08 17:45:47 -0300

    teste

---
+++ src/main/java/com/springboot/springboot/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.springboot.springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
